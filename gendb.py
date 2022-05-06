768c43c05ba6ba296c22ef138539e7d28a519f15
MassimilianoZuin <zuinmassimiliano@gmail.com>
2022-05-06 10:40:33 +0200

    Update genbd.py
    Inizio scrittura DB finale
    Rinominato DB di prova

--- gendb.py
+++ gendb.py
@@ -3,10 +3,19 @@ from sqlalchemy import *
 engine = create_engine('sqlite:///database.db', echo = True)
 metadata = MetaData()
 
-users = Table('Users', metadata,
-            Column('id', Integer, primary_key=True),
-            Column('email', String),
-            Column('pwd', String)
+users = Table('Utenti', metadata,
+            Column('Email', String, primary_key=True, index=True), #index singoli. Per index multicolonna serve crearli dopo l'ultima colonna
+            Column('Nome', String, nullable=False), #mettiamo un limite alla grandezza?
+            Column('Cognome', String, nullable=False), #mettiamo un limite alla grandezza?
+            Column('Nickname', String(24), nullable=True, unique=True), #mettiamo un limite alla grandezza? Unique nullable?
+            Column('Data_Nascita', Date, nullable=False),
+            Column('Password', String(16), nullable=False) #mettiamo un limite alla grandezza?
+        )
+
+artist = Table('Artisti', metadata,
+            Column('Id_Utente', String, ForeignKey("Utenti.Email", onupdate="CASCADE", ondelete="CASCADE"), primary_key=True),
+            Column('Bio', String(160)), #mettiamo un limite alla grandezza?
+            Column('Debutto', Date) #trigger per la prima canzone caricata?
         )
 
 metadata.create_all(engine)
@@ -19,3 +28,16 @@ conn.execute(ins, ['2', 'bob@gmail.com', 'milk'])
 user_list = conn.execute("SELECT * FROM Users")
 for u in user_list:
     print(u)
+
+
+
+"""
+users = Table('Utenti', metadata,
+            Column('', , primary_key=True),
+            Column('', ),
+            Column('', ),
+            Column('', ),
+            Column('', ),
+            Column('', )
+        )
+"""
